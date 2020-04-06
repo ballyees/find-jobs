bcc6ef44cf81de37d1deb5d9fe5162647645812c
ballyees <46591173+ballyees@users.noreply.github.com>
2020-04-06 20:09:42 +0700

    edit

--- precache-manifest.46233b4deb94d1c84082c09dee2f3ec9.js
+++ precache-manifest.46233b4deb94d1c84082c09dee2f3ec9.js
@@ -1,30 +1,29 @@
-self.__precacheManifest = (self.__precacheManifest || []).concat([
-  {
-    "revision": "49150e9d37eed1fbc5a142013bbe36f0",
-    "url": "/ballyees/find-jobs.git/index.html"
-  },
-  {
-    "revision": "146aa099c99e70c9faef",
-    "url": "/ballyees/find-jobs.git/static/css/main.d4bca1f4.chunk.css"
-  },
-  {
-    "revision": "807f0a5e0c705920a0fa",
-    "url": "/ballyees/find-jobs.git/static/js/2.8a46a464.chunk.js"
-  },
-  {
-    "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/ballyees/find-jobs.git/static/js/2.8a46a464.chunk.js.LICENSE.txt"
-  },
-  {
-    "revision": "146aa099c99e70c9faef",
-    "url": "/ballyees/find-jobs.git/static/js/main.b844bba7.chunk.js"
-  },
-  {
-    "revision": "3479d7ab496710e9f6c3",
-    "url": "/ballyees/find-jobs.git/static/js/runtime-main.f6ded3bf.js"
-  },
-  {
-    "revision": "a0ece36f566c10116cbc6f7818fba41d",
-    "url": "/ballyees/find-jobs.git/static/media/young-college-students.a0ece36f.jpg"
-  }
+self.__precacheManifest = (self.__precacheManifest || []).concat([{
+        "revision": "49150e9d37eed1fbc5a142013bbe36f0",
+        "url": "index.html"
+    },
+    {
+        "revision": "146aa099c99e70c9faef",
+        "url": "static/css/main.d4bca1f4.chunk.css"
+    },
+    {
+        "revision": "807f0a5e0c705920a0fa",
+        "url": "static/js/2.8a46a464.chunk.js"
+    },
+    {
+        "revision": "c64c486544348f10a6d6c716950bc223",
+        "url": "static/js/2.8a46a464.chunk.js.LICENSE.txt"
+    },
+    {
+        "revision": "146aa099c99e70c9faef",
+        "url": "static/js/main.b844bba7.chunk.js"
+    },
+    {
+        "revision": "3479d7ab496710e9f6c3",
+        "url": "static/js/runtime-main.f6ded3bf.js"
+    },
+    {
+        "revision": "a0ece36f566c10116cbc6f7818fba41d",
+        "url": "static/media/young-college-students.a0ece36f.jpg"
+    }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,13 +14,13 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ballyees/find-jobs.git/precache-manifest.46233b4deb94d1c84082c09dee2f3ec9.js"
+    "precache-manifest.46233b4deb94d1c84082c09dee2f3ec9.js"
 );
 
 self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
 });
 
 workbox.core.clientsClaim();
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ballyees/find-jobs.git/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"), {
+
+    blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/],
 });
